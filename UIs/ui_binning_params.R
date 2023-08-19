5d82fa77da690200fc6585460d57f5303fa7a598
Elisa Loy <37096340+loye16@users.noreply.github.com>
2023-08-19 12:00:05 -0400

    Update back end of binning params

--- UIs/ui_binning_params.R
+++ UIs/ui_binning_params.R
@@ -14,18 +14,21 @@ binning_params <- tabPanel(
                  helpText("Loaded raster data: "),
                  textOutput("bin_show_chosen_raster")),
              shinydashboard::box(width = 8,
+                 html2Text("Required Parameters:"),
                  numericInput("bin_bin_width", "Bin width", value = 10, min = 1),
                  numericInput("bin_step_size", "Step size", value = 1, min = 1),
-                 numericInput("bin_start_ind",
-                              "Index of the sample where the first bin starts (optional)",
+                 textInput("bin_file_name_prefix",
+                           "Prefix of binned file name (e.g., project abbr.)"),
+                 html2Text("Optional Parameters:"),
+                 numericInput("bin_start_time",
+                              "Index of the sample where the first bin starts",
                               value = NULL),
-                 numericInput("bin_end_ind",
-                              "Index of the sample where the last bin ends (optional)",
+                 numericInput("bin_end_time",
+                              "Index of the sample where the last bin ends",
                               value = NULL),
-                 textInput("bin_prefix_of_binned_file_name",
-                           "prefix of binned file name (e.g., ZD)"),
                  actionButton("bin_the_data", "Bin the data"),
-                 textOutput("bin_show_create_bin_function_run")
+                 html2Text("The following code was most recently run:"),
+                 htmlOutput("show_binning_function")
              )
       )
     )
--- servers/server_binning_params.R
+++ servers/server_binning_params.R
@@ -8,70 +8,84 @@ shinyFiles::shinyDirChoose(input, "bin_chosen_raster",
                            roots = c(wd=file.path('data', 'raster')),
                            filetypes = c("mat", "Rda"))
 
+# Setting the file name and list of rasters
+observe({
+  req(input$bin_chosen_raster)
+  # Set current directory name to the base directory
+  rv$selected_rasters <- shinyFiles::parseDirPath(c(wd=rv$raster_base_dir), input$bin_chosen_raster)
+  rv$mat_raster_list <- list.files(rv$selected_rasters, pattern = "\\.mat$")
+  rv$rda_raster_list <- list.files(rv$selected_rasters, pattern = "\\.[rR]da$")
+  rv$raster_num_neuron <- length(rv$rda_raster_list)
+})
+
+
 # Display raster name
 output$bin_show_chosen_raster <- renderText({
-  req(rv$raster_cur_dir_name)
-  if(is.na(rv$raster_cur_dir_name)){
+  req(rv$selected_rasters)
+  if(is.na(rv$selected_rasters)){
     "No file chosen yet"
   } else{
-    rv$raster_cur_dir_name
-
+    rv$selected_rasters
   }
 })
+
 
 ################################################################################
 ############################### Binning the data ###############################
 ################################################################################
 
-# Run binning if button is pressed
-# Elisa - a lot needs to change here and then commented on
+# Create default prefix for binning
+reactive_bin_prefix <- reactive({
+  req(rv$working_dir)
+  # First three letters of project name
+  toupper(substr(basename(rv$working_dir), 1, 3))
+})
+
+# Update the bin prefix default value in the ui
+observe({
+  updateTextInput(session, "bin_file_name_prefix", value = reactive_bin_prefix())
+})
+
+
+# Run binning if button is pressed and creating display text
 observeEvent(input$bin_the_data,{
-  if(rv$raster_bRda){
-    # elisa data binned data in the director data/binned
-    #binned_basename <- trimws(file.path("data", "binned", " "))
+  req(input$bin_bin_width, input$bin_step_size,
+      rv$selected_rasters, input$bin_file_name_prefix)
 
-    temp_call <- paste0("NeuroDecodeR:::create_binned_data(rv$raster_cur_dir_name, ",
-                       "paste0(binned_base_dir, input$bin_prefix_of_binned_file_name),",
-                       "input$bin_bin_width, input$bin_step_size")
+  # Location of new binned file
+  binned_name <- paste0(rv$binned_base_dir, "/", input$bin_file_name_prefix)
 
-    if(!is.na(input$bin_start_ind)){
-      temp_call <- paste0(temp_call, ",input$bin_start_ind")
-    }
-    if(!is.na(input$bin_end_ind)){
-      temp_call <- paste0(temp_call, ",input$bin_end_ind")
-    }
-    temp_call <- paste0(temp_call,")")
-    #rv$create_bin_function_run <- temp_call
+  # To call the function
+  decoder_call <- paste0("NeuroDecodeR:::create_binned_data(rv$selected_rasters, ",
+                         "binned_name, input$bin_bin_width, input$bin_step_size")
 
-    rv$create_bin_function_run <- paste0("NeuroDecodeR:::create_binned_data('",
-                                         rv$raster_cur_dir_name, "', ",
-                                         #"'", binned_basename,
-                                         "'", ,
-                                         input$bin_prefix_of_binned_file_name, "', ",
-                                         input$bin_bin_width, ", ", input$bin_step_size, ")")
+  # To show the function in the ui
+  rv$create_bin_function <- list(paste0("'", rv$selected_rasters,"'"),
+                                 paste0("'", binned_name, "'"),
+                                 input$bin_bin_width, input$bin_step_size)
 
-    eval(parse(text = temp_call))
-
-  } else if(rv$raster_bMat){
-    temp_call <- paste0("NeuroDecodeR:::create_binned_data_from_matlab_raster_data(rv$raster_cur_dir_name,",
-                       "input$bin_prefix_of_binned_file_name,",
-                       "input$bin_bin_width, input$bin_step_size")
-    if(!is.na(input$bin_start_ind)){
-      temp_call <- paste0(temp_call, ",input$bin_start_ind")
-    }
-    if(!is.na(input$bin_end_ind)){
-      temp_call <- paste0(temp_call, ",input$bin_end_ind")
-    }
-    temp_call <- paste0(temp_call,")")
-    rv$create_bin_function_run <- temp_call
-    eval(parse(text = temp_call))
-
+  # Add optional values if entered
+  if(!is.na(input$bin_start_time)){
+    decoder_call <- paste0(decoder_call, ", input$bin_start_time")
+    rv$create_bin_function <- paste0(rv$create_bin_function, input$bin_start_time)
   }
+  if(!is.na(input$bin_end_time)){
+    decoder_call <- paste0(decoder_call, ", input$bin_end_time")
+    rv$create_bin_function <- paste0(rv$create_bin_function, input$bin_end_time)
+  }
+
+  # Complete the functions
+  decoder_call <- paste0(decoder_call,")")
+  rv$create_bin_function  <- paste0("NeuroDecodeR:::create_binned_data(",
+                   paste(rv$create_bin_function, collapse = ", "), ")")
+
+  # Evaluate the code
+  eval(parse(text = decoder_call))
 
 })
 
-# Display bin name? elisa - maybe change the name
-output$bin_show_create_bin_function_run <- renderText({
-  rv$create_bin_function_run
+# Display bin function run
+output$show_binning_function <- renderText({
+  rv$create_bin_function
 })
 
